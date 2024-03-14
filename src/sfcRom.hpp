a4dbd203c741e0abd3bb8a61e2ca56d09cf3cfd7
David Lindecrantz <optiroc@me.com>
2024-03-14 23:52:16 +0100

    Set up for unit tests

--- src/sfcRom.hpp
+++ src/sfcRom.hpp
@@ -7,9 +7,17 @@
 struct sfcRom {
   sfcRom(const std::string& path);
 
-  bool isValid() const;
   std::string description(bool silent) const;
   std::string fix(const std::string& path, bool silent);
+
+  bool isValid() const { return _isValid; }
+  bool hasIssues() const { return _hasIssues; }
+  bool hasCopierHeader() const { return _hasCopierHeader; }
+  bool hasCorrectTitle() const { return _hasCorrectTitle; }
+  bool hasCorrectRamSize() const { return _hasCorrectRamSize; }
+  bool hasLegalMode() const { return _hasLegalMode; }
+  bool hasKnownMapper() const { return _hasKnownMapper; }
+  bool hasNewFormatHeader() const { return _hasNewFormatHeader; }
 
 private:
   std::string filepath;
@@ -21,18 +29,18 @@ private:
 
   size_t imageSize;
   size_t imageOffset = 0;
-  size_t headerLocation;
+  size_t headerLocation = 0;
 
-  bool valid = false;
-  bool hasIssues = false;
-  bool hasSevereIssues = false;
+  bool _isValid = false;
+  bool _hasIssues = false;
+  bool _hasSevereIssues = false;
 
-  bool hasCopierHeader = false;
-  bool hasCorrectTitle = false;
-  bool hasCorrectRamSize = true;
-  bool hasLegalMode = true;
-  bool hasKnownMapper = true;
-  bool hasNewFormatHeader = false;
+  bool _hasCopierHeader = false;
+  bool _hasCorrectTitle = false;
+  bool _hasCorrectRamSize = true;
+  bool _hasLegalMode = true;
+  bool _hasKnownMapper = true;
+  bool _hasNewFormatHeader = false;
 
   uint8_t correctedMode = 0;
   uint8_t correctedRomSize = 0;
---
+++ test/test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+#include <catch2/catch_test_macros.hpp>
+#include "../src/sfcRom.hpp"
+
+// rom0.sfc:
+//   - 32Kb, code at 0x0000, mapped at 0x8000
+//  - Reset vector 0x0000
+//  - Not valid
+inline constexpr auto rom0 = "data/rom0.sfc";
+
+// rom1.sfc:
+//   - 32Kb
+//   - Valid sfc
+//   - ROM size 0x05
+//   - Map mode 0x00
+//   - Chipset 0x00
+//   - Speed 200ns
+//   - Bad checksum
+inline constexpr auto rom1 = "data/rom1.sfc";
+
+bool rom_isValid(const std::string& path) {
+  std::cout  << '\n' << "--- Loading: " << path << '\n';
+  sfcRom rom(path);
+  std::cout << "--- Loaded: " << path << '\n';
+  return rom.isValid();
+}
+
+TEST_CASE("sfcRom.isValid") {
+  REQUIRE(rom_isValid(rom1) == true);
+  REQUIRE(rom_isValid(rom0) == false);
+}
